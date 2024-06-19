af8ebe0effc9834e8df60f4db690cabd75a36aca
bartMn <bartoszmania.bm+github@gmail.com>
2024-06-19 01:02:44 +0100

    updated data paths in create_flow_imgs.py

--- src/data/create_flow_imgs.py
+++ src/data/create_flow_imgs.py
@@ -7,7 +7,7 @@ import threading
 
 
 def get_flow_and_save(source, dest, env_num, cam_num):
-    frame1_source = source +os.sep+ f"env{env_num}_cam{cam_num}_frame0.png"
+    frame1_source = source +os.sep+ f"cam{cam_num}{os.sep}env{env_num}{os.sep}frame0.png"
     #frame2_source = source +os.sep+ "rgb_env0_cam0_frame20.png"
 
     frame1 = cv.imread(frame1_source)
@@ -19,7 +19,7 @@ def get_flow_and_save(source, dest, env_num, cam_num):
     hsv[..., 1] = 255
 
     for i in tqdm(range(1, 121)):
-        frame2_source = source +os.sep+ f"env{env_num}_cam{cam_num}_frame{i}.png"
+        frame2_source = source +os.sep+ f"cam{cam_num}{os.sep}env{env_num}{os.sep}frame{i}.png"
 
         frame2 = cv.imread(frame2_source)
         next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
@@ -29,15 +29,15 @@ def get_flow_and_save(source, dest, env_num, cam_num):
         hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
         bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
 
-        cv.imwrite(f'{dest+os.sep}env{env_num}_cam{cam_num}_frame{i}.png', bgr)
+        cv.imwrite(f'{dest+os.sep}cam{cam_num}{os.sep}env{env_num}{os.sep}frame{i}.png', bgr)
         prvs = next
 
 
 
 def main():
     camera_env_combunations = list(product([env_n for env_n in range(2)], [cam_n for cam_n in range(5)]))
-    source = "/home/bart/project/IsaacGym_Preview_4_Package/isaacgym/IsaacGymEnvs-main/isaacgymenvs/recorded_data/cam_data_rgb"
-    dest = "/home/bart/project/IsaacGym_Preview_4_Package/isaacgym/IsaacGymEnvs-main/isaacgymenvs/recorded_data/cam_data_flow"
+    source = "/home/bart/project/IsaacGym_Preview_4_Package/isaacgym/IsaacGymEnvs-main/isaacgymenvs/recorded_data/cameras/rgb"
+    dest = "/home/bart/project/IsaacGym_Preview_4_Package/isaacgym/IsaacGymEnvs-main/isaacgymenvs/recorded_data/cameras/flow"
 
     for env_num, cam_num in tqdm(camera_env_combunations):
         #get_flow_and_save(source, dest, env_num, cam_num)
